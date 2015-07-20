ad8a325a186968d4ab55ce5b20ca92ff3ee27a0a
Carson Yong <CarsonYong@gmail.com>
2015-07-20 15:59:26 +0000

    Adding basic music post features

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,27 @@
 get '/' do
   erb :index
 end
+
+
+# get '/musicwall' do
+#   'Music List here'
+# end
+
+get '/musicwall' do
+  @songs = Song.all
+  erb :'musicwall/index'
+end
+
+get '/musicwall/new' do
+  erb :'musicwall/new'
+end
+
+post '/musicwall' do
+  @songs = Song.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  @songs.save
+  redirect '/musicwall'
+end
